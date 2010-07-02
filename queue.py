994606ef68d688fe61fbd699ce750e9f695e7053
Henry Robinson <henry@cloudera.com>
2010-07-01 17:57:26 -0700

    Add BSD license, change formatting to two-space-tabs and comment methods.

--- queue.py
+++ queue.py
@@ -1,84 +1,132 @@
+# Copyright (c) 2010, Henry Robinson
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import zookeeper, threading, sys
 ZOO_OPEN_ACL_UNSAFE = {"perms":0x1f, "scheme":"world", "id" :"anyone"};
 
 class ZooKeeperQueue(object):
-    def __init__(self,queuename):
-        self.connected = False
-        self.queuename = "/" + queuename
-        self.cv = threading.Condition()
-        zookeeper.set_log_stream(open("/dev/null"))
-        def watcher(handle,type,state,path):
-            print "Connected"
-            self.cv.acquire()
-            self.connected = True
-            self.cv.notify()
-            self.cv.release()
+  """
+  This is a distributed queue implementation using Apache ZooKeeper.
 
-        self.cv.acquire()
-        self.handle = zookeeper.init("localhost:2181", watcher, 10000)
-        self.cv.wait(10.0)
-        if not self.connected:
-            print "Connection to ZooKeeper cluster timed out - is a server running on localhost:2181?"
-            sys.exit()
+  See this blog post:
+  http://www.cloudera.com/blog/2009/05/building-a-distributed-concurrent-queue-with-apache-zookeeper/
+
+  for more details.
+  """
+  def __init__(self,queuename):
+    self.connected = False
+    self.queuename = "/" + queuename
+    self.cv = threading.Condition()
+    zookeeper.set_log_stream(open("/dev/null"))
+    def watcher(handle,type,state,path):
+      print "Connected"
+      self.cv.acquire()
+      self.connected = True
+      self.cv.notify()
+      self.cv.release()
+
+    self.cv.acquire()
+    self.handle = zookeeper.init("localhost:2181", watcher, 10000)
+    self.cv.wait(10.0)
+    if not self.connected:
+      print "Connection to ZooKeeper cluster timed out - is a server running on localhost:2181?"
+      sys.exit()
+    self.cv.release()
+    try:
+      zookeeper.create(self.handle,self.queuename,"queue top level", [ZOO_OPEN_ACL_UNSAFE],0)
+    except IOError, e:
+      if e.message == zookeeper.zerror(zookeeper.NODEEXISTS):
+        print "Queue already exists"
+      else:
+        raise e
+
+  def enqueue(self,val):
+    """
+    Adds a new znode whose contents are val to the queue
+    """
+    zookeeper.create(self.handle, self.queuename+"/item", val, [ZOO_OPEN_ACL_UNSAFE],zookeeper.SEQUENCE)
+
+  def dequeue(self):
+    """
+    Removes an item from the queue. Returns None is the queue is empty
+    when it is read.
+    """
+    while True:
+      children = sorted(zookeeper.get_children(self.handle, self.queuename,None))
+      if len(children) == 0:
+        return None
+      for child in children:
+        data = self.get_and_delete(self.queuename + "/" + children[0])
+        if data:
+          return data
+
+  def get_and_delete(self,node):
+    """
+    Atomic get-and-delete operation that retries until it is able to
+    delete the same version that it read.
+    """
+    try:
+      (data,stat) = zookeeper.get(self.handle, node, None)
+      zookeeper.delete(self.handle, node, stat["version"])
+      return data
+    except IOError, e:
+      if e.message == zookeeper.zerror(zookeeper.NONODE):
+        return None
+      raise e
+
+  def block_dequeue(self):
+    """
+    Similar to dequeue, but if the queue is empty, block until an item
+    is added and successfully removed.
+    """
+    def queue_watcher(handle,event,state,path):
+      self.cv.acquire()
+      self.cv.notify()
+      self.cv.release()
+    while True:
+      self.cv.acquire()
+      children = sorted(zookeeper.get_children(self.handle, self.queuename, queue_watcher))
+      for child in children:
+        data = self.get_and_delete(self.queuename+"/"+children[0])
+        if data != None:
+          self.cv.release()
+          return data
+        self.cv.wait()
         self.cv.release()
-        try:
-            zookeeper.create(self.handle,self.queuename,"queue top level", [ZOO_OPEN_ACL_UNSAFE],0)
-        except IOError, e:
-            if e.message == zookeeper.zerror(zookeeper.NODEEXISTS):
-                print "Queue already exists"
-            else:
-                raise e
-
-    def enqueue(self,val):
-        zookeeper.create(self.handle, self.queuename+"/item", val, [ZOO_OPEN_ACL_UNSAFE],zookeeper.SEQUENCE)
-
-    def dequeue(self):
-        while True:
-            children = sorted(zookeeper.get_children(self.handle, self.queuename,None))
-            if len(children) == 0:
-                return None
-            for child in children:
-                data = self.get_and_delete(self.queuename + "/" + children[0])
-                if data:
-                    return data
-
-    def get_and_delete(self,node):
-        try:
-            (data,stat) = zookeeper.get(self.handle, node, None)
-            zookeeper.delete(self.handle, node, stat["version"])
-            return data
-        except IOError, e:
-            if e.message == zookeeper.zerror(zookeeper.NONODE):
-                return None
-            raise e
-
-    def block_dequeue(self):
-        def queue_watcher(handle,event,state,path):
-            self.cv.acquire()
-            self.cv.notify()
-            self.cv.release()
-        while True:
-            self.cv.acquire()
-            children = sorted(zookeeper.get_children(self.handle, self.queuename, queue_watcher))
-            for child in children:
-                data = self.get_and_delete(self.queuename+"/"+children[0])
-                if data != None:
-                    self.cv.release()
-                    return data
-            self.cv.wait()
-            self.cv.release()
 
 if __name__ == '__main__':
-    zk = ZooKeeperQueue("myfirstqueue")
-    print "Enqueuing three items"
-    zk.enqueue("queue item 1")
-    zk.enqueue("queue item 2")
-    zk.enqueue("queue item 3")
-    print "Done"
-
-    print "Consuming all items in queue"
+  zk = ZooKeeperQueue("myfirstqueue")
+  print "Enqueuing three items"
+  zk.enqueue("queue item 1")
+  zk.enqueue("queue item 2")
+  zk.enqueue("queue item 3")
+  print "Done"
+  
+  print "Consuming all items in queue"
+  v = zk.dequeue()
+  while v != None:
+    print v
     v = zk.dequeue()
-    while v != None:
-        print v
-        v = zk.dequeue()
-    print "Done"
+  print "Done"
